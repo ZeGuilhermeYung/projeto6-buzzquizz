274267cca5b0bdc005285c8815fac9d6d99722c5
Shizuke136 <romanjose3008@gmail.com>
2022-06-29 20:42:14 -0300

    Tela de adicionar perguntas adicionada

--- script.js
+++ script.js
@@ -98,5 +98,50 @@ function displayQuizz (quizz) {
 
 
 /*Js perguntas*/
+function questionMaker(){
+    const quizzDetails = document.querySelectorAll("input")
+    let preQuizz = {title:"",image:"",numberOfQuestions:"",numberOfLevels:""}
 
-console.log(document.querySelectorAll(".firstUl"))
+    preQuizz.title = quizzDetails[0].value
+    preQuizz.image = quizzDetails[1].value
+    preQuizz.numberOfQuestions = Number(quizzDetails[2].value)
+    preQuizz.numberOfLevels = Number(quizzDetails[3].value)
+
+    const screen = document.querySelector(".screen3")
+    screen.classList.add("hidden")
+
+    const newScreen = document.querySelector(".screen3_2")
+    newScreen.classList.remove("hidden")
+
+    const questionList = document.querySelector(".secondUl")
+
+    for(let i = 0; i < preQuizz.numberOfQuestions; i++){
+        questionList.innerHTML += `
+                    <ul>
+                        <li><h1>Pergunta ${i + 1}</h1></li>
+                        <li><input type="text" placeholder="Texto da pergunta" required></li>
+                        <li><input type="text" placeholder="Cor de fundo da pergunta" required></li>
+                    </ul>
+                    <ul>
+                        <li><h1>Resposta Correta</h1></li>                    
+                        <li><input type="text" placeholder="Resposta Correta" required></li>
+                        <li><input type="text" placeholder="URL da Imagem"required></li>
+                        </ul>
+                    <ul>
+                        <li><h1>Respostas Incorretas</h1></li>
+                        <li><input type="text" placeholder="Resposta Correta" required></li>
+                        <li><input type="text" placeholder="URL da Imagem"required></li>
+                        <li> <br><br></li>
+                    </ul>
+                    <ul>
+                        <li><input type="text" placeholder="Resposta Correta" required></li>
+                        <li><input type="text" placeholder="URL da Imagem"required></li>
+                        <li><br><br></li>
+                    </ul>
+                    <ul>
+                        <li><input type="text" placeholder="Resposta Correta" required></li>
+                        <li><input type="text" placeholder="URL da Imagem"required></li>
+                    </ul>
+            `
+    }
+}
