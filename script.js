bb34dfa02b7cf6bbcfbddf094120c657bbc0d0e3
Shizuke136 <romanjose3008@gmail.com>
2022-06-29 14:50:40 -0300

    Update index.html

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ allQuizz.then(renderQuizz)
 
 function renderQuizz(info){
     const quizzSpace = document.querySelector(".row")
-    
+    quizzSpace.innerHTML = ""
+
     for(let i = 0; i < info.data.length; i++){
-        quizzSpace.innerHTML += `<div class="divImg"><img class="image2" src="${info.data[i].image}"><p>${info.data[i].title}</p></div>`
+        quizzSpace.innerHTML += `<div id="${info.data[i].id}"class="divImg"><img class="image2" src="${info.data[i].image}"><p>${info.data[i].title}</p></div>`
     }
 }
