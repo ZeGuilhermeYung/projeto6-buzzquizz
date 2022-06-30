9af1fb008738a1a61df23d72d09c8618dc44dfaf
Shizuke136 <romanjose3008@gmail.com>
2022-06-30 09:17:13 -0300

    new update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function getAllQuizzOptions() {
-  const allQuizz = axios.get(
-    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
-  );
-  allQuizz.then(renderQuizzOptions);
-  //allQuizz.catch(catchQuizzOptions);
+function getAllQuizzOptions () {
+    const allQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    allQuizz.then (renderQuizzOptions);
+    //allQuizz.catch(catchQuizzOptions);
 }
 
 getAllQuizzOptions();
@@ -16,7 +14,7 @@ function renderQuizzOptions (info) {
             <img class="image2" src="${info.data[i].image}">
             <p>${info.data[i].title}</p>
         </div>`;
-  }
+    }
 }
 
 function getQuizz (quizz) {
@@ -31,30 +29,29 @@ function displayQuizz (selectedQuizz) {
     <div class="banner" style="background-image: ${backImg};">
         <h1>${selectedQuizz.data.title}</h1>
     </div>`;
-  selectedQuizz.data.questions.sort(scrambleAlternatives);
-  for (let i = 0; i < selectedQuizz.data.questions.length; i++) {
-    document.querySelector(".screen2").innerHTML += `
+    selectedQuizz.data.questions.sort(scrambleAlternatives);
+    for (let i = 0; i < selectedQuizz.data.questions.length; i++) {
+        document.querySelector(".screen2").innerHTML += `
         <div class="question">
             <div class="question-title" style="background-color=${selectedQuizz.data.questions[i].color}">
                 <h2>${selectedQuizz.data.questions[i].title}</h2>
             </div>
             <div class="answer-options">`;
-    selectedQuizz.data.questions[i].answers.sort(scrambleAlternatives);
-    for (let j = 0; j < 4; j++) {
-      let isRightOrWrong = isCorrect(
-        selectedQuizz.data.questions[i].answers[j].isCorrectAnswer
-      );
-      document.querySelector(".answer-options").innerHTML += `
+        selectedQuizz.data.questions[i].answers.sort(scrambleAlternatives);
+        for (let j = 0; j < 4; j++) {
+            let isRightOrWrong = isCorrect(selectedQuizz.data.questions[i].answers[j].isCorrectAnswer);
+            document.querySelector(".answer-options").innerHTML += `
             <div class="alternative ${isRightOrWrong}">
                 <img src="${selectedQuizz.data.questions[i].answers[j].image}" alt="">
                 <h3>${selectedQuizz.data.questions[i].answers[j].text}</h3>
             </div>`;
+        }  
     }
-  }
-  document.querySelector(".screen2").innerHTML += `</div>
+    document.querySelector(".screen2").innerHTML +=
+        `</div>
     </div>`;
-  document.querySelector(".screen1").classList.add("hidden");
-  document.querySelector(".screen2.hidden").classList.remove("hidden");
+    document.querySelector(".screen1").classList.add("hidden");
+    document.querySelector(".screen2.hidden").classList.remove("hidden");
 }
 
 function scrambleAlternatives () { 
@@ -62,38 +59,35 @@ function scrambleAlternatives () {
 }
 
 function isCorrect(alternative) {
-  if (alternative === true) {
-    return "right";
-  } else {
-    return "wrong";
-  }
+    if (alternative === true) {
+        return "right";
+    } else {
+        return "wrong";
+    }
 }
 //Js Perguntas
+let obj
 
-function questionMaker() {
-  const quizzDetails = document.querySelectorAll("input");
-  let preQuizz = {
-    title: "",
-    image: "",
-    numberOfQuestions: "",
-    numberOfLevels: "",
-  };
+function questionMaker(){
+    const quizzDetails = document.querySelectorAll(".preQuizz")
+    let preQuizz = {title:"",image:"",numberOfQuestions:"",numberOfLevels:""}
+    obj = preQuizz
 
-  preQuizz.title = quizzDetails[0].value;
-  preQuizz.image = quizzDetails[1].value;
-  preQuizz.numberOfQuestions = Number(quizzDetails[2].value);
-  preQuizz.numberOfLevels = Number(quizzDetails[3].value);
+    preQuizz.title = quizzDetails[0].value;
+    preQuizz.image = quizzDetails[1].value;
+    preQuizz.numberOfQuestions = Number(quizzDetails[2].value);
+    preQuizz.numberOfLevels = Number(quizzDetails[3].value);
+
+    const screen = document.querySelector(".screen3");
+    screen.classList.add("hidden");
 
     const newScreen = document.querySelector(".screen3_2");
     newScreen.classList.remove("hidden");   
 
-  const newScreen = document.querySelector(".screen3_2");
-  newScreen.classList.remove("hidden");
+    const questionList = document.querySelector(".secondUl");
 
-  const questionList = document.querySelector(".secondUl");
-
-  for (let i = 0; i < preQuizz.numberOfQuestions; i++) {
-    questionList.innerHTML += `
+    for(let i = 0; i < preQuizz.numberOfQuestions; i++){
+        questionList.innerHTML += `
                     <ul>
                         <li><h1>Pergunta ${i + 1}</h1></li>
                         <li><input class="question" type="text" placeholder="Texto da pergunta" required></li>
@@ -120,11 +114,40 @@ function questionMaker() {
                         <li><input class="question" type="text" placeholder="URL da Imagem"required></li>
                     </ul>
             `;
-  }
+    }
+}
+
+
+
+
+
+function levelMaker(){
+
+    levels = obj.numberOfLevels
+
+    const screen = document.querySelector(".screen3_2")
+    screen.classList.add("hidden")
+
+    const newScreen = document.querySelector(".screen3_3")
+    newScreen.classList.remove("hidden")
+
+    const levelList = document.querySelector(".thirdUl")
+
+    for(let i = 0; i < levels; i++){
+        levelList.innerHTML += `
+        <ul>
+            <li><h1>Nível ${i + 1}</h1></li>
+            <li><input class="level" type="text" placeholder="Título do nível" required></li>
+            <li><input class="level" type="text" placeholder="% de acerto mínima" required></li>
+            <li><input class="level" type="text" placeholder="URL da imagem do nível" required></li>
+            <li><input class="level" type="text" placeholder="Descrição do nível" required></li>
+        </ul>
+        `
+    }
 }
 
 function validateURL(url) {
-  const rule =
-    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-  return rule.test(url);
+    const rule =
+      /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+    return rule.test(url);
 }
