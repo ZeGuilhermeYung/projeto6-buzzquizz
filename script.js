d92dda5c232decfd5552fced67b7bb4138690f71
Juliana Satoko Hagiwara Miura <hagiwaramiura.satoko@gmail.com>
2022-06-30 08:25:21 -0300

    Create function to validate URL

--- script.js
+++ script.js
@@ -1,84 +1,95 @@
-function getAllQuizzOptions () {
-    const allQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    allQuizz.then (renderQuizzOptions);
-    //allQuizz.catch(catchQuizzOptions);
+function getAllQuizzOptions() {
+  const allQuizz = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+  allQuizz.then(renderQuizzOptions);
+  //allQuizz.catch(catchQuizzOptions);
 }
 getAllQuizzOptions();
-function renderQuizzOptions (info) {
-    const quizzSpace = document.querySelector(".row");   
-    for (let i = 0; i < info.data.length; i++) {
-        quizzSpace.innerHTML += `
+function renderQuizzOptions(info) {
+  const quizzSpace = document.querySelector(".row");
+  for (let i = 0; i < info.data.length; i++) {
+    quizzSpace.innerHTML += `
         <div class="divImg" id="${info.data[i].id}" onclick="getQuizz(this);">
             <img class="image2" src="${info.data[i].image}">
             <p>${info.data[i].title}</p>
         </div>`;
-    }
+  }
 }
-function getQuizz (quizz) {
-    const id = quizz.id;
-    const quizzID = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
-    quizzID.then (displayQuizz);
+function getQuizz(quizz) {
+  const id = quizz.id;
+  const quizzID = axios.get(
+    `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`
+  );
+  quizzID.then(displayQuizz);
 }
-function displayQuizz (selectedQuizz) {
-    const backImg = `url(${selectedQuizz.data.image})`;
-    document.querySelector(".screen2").innerHTML = `
+function displayQuizz(selectedQuizz) {
+  const backImg = `url(${selectedQuizz.data.image})`;
+  document.querySelector(".screen2").innerHTML = `
     <div class="banner" style="background-image: ${backImg};">
         <h1>${selectedQuizz.data.title}</h1>
     </div>`;
-    selectedQuizz.data.questions.sort(scrambleAlternatives);
-    for (let i = 0; i < selectedQuizz.data.questions.length; i++) {
-        document.querySelector(".screen2").innerHTML += `
+  selectedQuizz.data.questions.sort(scrambleAlternatives);
+  for (let i = 0; i < selectedQuizz.data.questions.length; i++) {
+    document.querySelector(".screen2").innerHTML += `
         <div class="question">
             <div class="question-title" style="background-color=${selectedQuizz.data.questions[i].color}">
                 <h2>${selectedQuizz.data.questions[i].title}</h2>
             </div>
             <div class="answer-options">`;
-        selectedQuizz.data.questions[i].answers.sort(scrambleAlternatives);
-        for (let j = 0; j < 4; j++) {
-            let isRightOrWrong = isCorrect(selectedQuizz.data.questions[i].answers[j].isCorrectAnswer);
-            document.querySelector(".answer-options").innerHTML += `
+    selectedQuizz.data.questions[i].answers.sort(scrambleAlternatives);
+    for (let j = 0; j < 4; j++) {
+      let isRightOrWrong = isCorrect(
+        selectedQuizz.data.questions[i].answers[j].isCorrectAnswer
+      );
+      document.querySelector(".answer-options").innerHTML += `
             <div class="alternative ${isRightOrWrong}">
                 <img src="${selectedQuizz.data.questions[i].answers[j].image}" alt="">
                 <h3>${selectedQuizz.data.questions[i].answers[j].text}</h3>
             </div>`;
-        }  
     }
-    document.querySelector(".screen2").innerHTML +=
-        `</div>
+  }
+  document.querySelector(".screen2").innerHTML += `</div>
     </div>`;
-    document.querySelector(".screen1").classList.add("hidden");
-    document.querySelector(".screen2.hidden").classList.remove("hidden");
+  document.querySelector(".screen1").classList.add("hidden");
+  document.querySelector(".screen2.hidden").classList.remove("hidden");
 }
-function scrambleAlternatives () { 
-	return Math.random() - 0.5; 
+function scrambleAlternatives() {
+  return Math.random() - 0.5;
 }
 function isCorrect(alternative) {
-    if (alternative === true) {
-        return "right";
-    } else {
-        return "wrong";
-    }
+  if (alternative === true) {
+    return "right";
+  } else {
+    return "wrong";
+  }
 }
 //Js Perguntas
-function questionMaker(){
-    const quizzDetails = document.querySelectorAll("input");
-    let preQuizz = {title:"",image:"",numberOfQuestions:"",numberOfLevels:""};
 
-    preQuizz.title = quizzDetails[0].value;
-    preQuizz.image = quizzDetails[1].value;
-    preQuizz.numberOfQuestions = Number(quizzDetails[2].value);
-    preQuizz.numberOfLevels = Number(quizzDetails[3].value);
+function questionMaker() {
+  const quizzDetails = document.querySelectorAll("input");
+  let preQuizz = {
+    title: "",
+    image: "",
+    numberOfQuestions: "",
+    numberOfLevels: "",
+  };
 
-    const screen = document.querySelector(".screen3");
-    screen.classList.add("hidden");
+  preQuizz.title = quizzDetails[0].value;
+  preQuizz.image = quizzDetails[1].value;
+  preQuizz.numberOfQuestions = Number(quizzDetails[2].value);
+  preQuizz.numberOfLevels = Number(quizzDetails[3].value);
 
-    const newScreen = document.querySelector(".screen3_2");
-    newScreen.classList.remove("hidden");
+  const screen = document.querySelector(".screen3");
+  screen.classList.add("hidden");
 
-    const questionList = document.querySelector(".secondUl");
+  const newScreen = document.querySelector(".screen3_2");
+  newScreen.classList.remove("hidden");
 
-    for(let i = 0; i < preQuizz.numberOfQuestions; i++){
-        questionList.innerHTML += `
+  const questionList = document.querySelector(".secondUl");
+
+  for (let i = 0; i < preQuizz.numberOfQuestions; i++) {
+    questionList.innerHTML += `
                     <ul>
                         <li><h1>Pergunta ${i + 1}</h1></li>
                         <li><input type="text" placeholder="Texto da pergunta" required></li>
@@ -105,5 +116,11 @@ function questionMaker(){
                         <li><input type="text" placeholder="URL da Imagem"required></li>
                     </ul>
             `;
-    }
+  }
+}
+
+function validateURL(url) {
+  const rule =
+    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+  return rule.test(url);
 }
